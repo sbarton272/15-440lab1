54a4f8e260c43d02c438c7c6a0e1de3b7fb4d78d
Spencer Barton <sbarton272@gmail.com>
2014-09-07 20:17:42 -0400

    Completed TransactionalOut

--- src/TransactionalFileOutputStream.java
+++ src/TransactionalFileOutputStream.java
@@ -1,13 +1,15 @@
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
 
 
 /**
- * This defines a thread-safe serializable file output stream.  
+ * This defines a thread-safe serializable file output stream.
  * 
  * @author Spencer
  *
@@ -17,42 +19,81 @@ public class TransactionalFileOutputStream extends OutputStream implements Seria
 	private static final long serialVersionUID = -3557579291611635226L;
 	private static final String SERIAL_PATH = "tmp";
 	private String serializedFileName;
-	private OutputStream inFile;
+	private OutputStream outFile;
 	
 	public TransactionalFileOutputStream(String fileName, boolean b) throws FileNotFoundException {
 		// TODO what is boolean for?
-		inFile = new FileOutputStream(fileName);
+		outFile = new FileOutputStream(fileName);
 		
 		// Generate unique file name for serialization of file descriptor
-		String uid = Integer.toString(inFile.hashCode());
+		String uid = Integer.toString(outFile.hashCode());
 		serializedFileName = new String(SERIAL_PATH + java.io.File.separator + uid);
+		
+		// Serialize outFile
+		serializeOutFile();
+		
+		// Close outFile so other processes can use it
+		try {
+			outFile.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	/**
+	 * Deserialize the outFile, write to it, reserialize and close the connection.
+	 */
 	@Override
 	public void write(int arg0) throws IOException {
-		// TODO Auto-generated method stub
-		// TODO: write, serialize, close, return
-
+		// TODO optimize by leaving open unless migrated
+		// TODO does process ever close?
+        
+        // Write to current serialzed outFile and serialize
+        deserializeOutFile();
+		outFile.write(arg0);
+		serializeOutFile();
+		
+		// Close file so it can be used by other processes
+		outFile.close();
 	}
 
 	/**
-	 * Serializes inFile by saving it to a unique serialization file.
+	 * Serializes outFile by saving it to a unique serialization file.
 	 */
-	private void serializeFileDescriptor() {
+	private void serializeOutFile() {
 		try {
 			// TODO add mutex?
 			FileOutputStream serializedFile = new FileOutputStream(serializedFileName);
 			ObjectOutputStream objStream = new ObjectOutputStream(serializedFile);
-			objStream.writeObject(inFile);
+			
+			// Serialize
+			objStream.writeObject(outFile);
+			
+	        // Close the serialization files
 			objStream.close();
 			serializedFile.close();
 			
-			// TODO debug
-	        System.out.printf("Serialized data is saved in " + serializedFileName);
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Deserializes the outFile by taking it from the serialized location
+	 */
+	private void deserializeOutFile() {
+		try {
+			FileInputStream serializedFile = new FileInputStream(serializedFileName);
+	        ObjectInputStream objStream = new ObjectInputStream(serializedFile);
+        
+			// Deserialize
+			outFile = (OutputStream) objStream.readObject();
+
+			// Close the serialization files
+			objStream.close();
+	        serializedFile.close();
+		
+		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
